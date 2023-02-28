ac13f783da40d42eac1acc4613c7daadc0734452
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-02-28 22:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your code here 
-
-
-
-
 function countWords() {
   var text = document.getElementById("evaluatedText").value;
-  var wordCount = text.trim().split(/\s+/).length;
-  document.getElementById("wordCount").textContent = wordCount;
+  if (text.trim() === "") {
+    document.getElementById("wordCount").textContent = 0;
+  } else {
+    var wordCount = text.trim().split(/\s+/).length;
+    document.getElementById("wordCount").textContent = wordCount;
+  }
 }
 
