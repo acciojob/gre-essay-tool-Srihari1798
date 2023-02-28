7a77e2691539e81f2012cc170548501e579c6d59
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-02-28 22:46:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+<textarea id="evaluatedText" oninput="countWords()"></textarea>
+<h3 id="wordCount">0</h3>
+
+<script>
+function countWords() {
+  var text = document.getElementById("evaluatedText").value;
+  var wordCount = text.trim().split(/\s+/).length;
+  document.getElementById("wordCount").textContent = wordCount;
+}
+</script>
